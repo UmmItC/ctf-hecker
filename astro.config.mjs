97249232ba606cea5672f03a9fb641554bbab4ac
UmmIt <git@ummit.dev>
2025-07-24 07:28:08 +0800

    feat: integrate Svelte framework support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-purple.vercel.app',
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: 'https://astro-purple.vercel.app',
+    integrations: [mdx(), sitemap(), svelte()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
